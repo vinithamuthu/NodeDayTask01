738a01f27c319a0533fbd9f6953ca788a1a2a10d
vinithamuthu <vinisurya.m@gmail.com>
2024-03-05 10:10:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,99 @@
+import express from "express";
+import fs from 'fs';
+import { format } from "date-fns";
+
+const app = express();
+
+// app.use() // middleware
+
+const PORT = 4000;  // http://localhost:4000/grtProducts
+
+
+app.get('/', (req, res) => {
+
+  res.status(200).json({ message: 'hai my dear team, we will become a FSD Developer' })
+
+})
+
+app.get('/get-data', (req, res) => {
+
+  res.status(200).json({ message: "data", data: { name: "hello world" } })
+
+})
+
+
+// app.get('/write', (req,res) => {
+// let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
+// console.log(('today' , today ));
+// const filePath = `TimeStamp/${today}.txt`
+// fs.writeFileSync(filePath, `${today}` , 'utf8' )
+// let data = fs.readFileSync(filePath , 'utf8')
+//   res.status(200).send(data)
+
+// })
+
+
+
+app.get('/write', async (req, res) => {
+
+  try {
+
+    let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
+    console.log(('today', today));
+    const filePath = `TimeStamp/${today}.txt`
+    await fs.writeFileSync(filePath, `${today}`, 'utf8')
+    res.status(200).send(`File ${today}.txt created successfully`)
+
+  } catch (error) {
+
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+
+  }
+
+})
+
+
+app.get('/read/:today', async (req, res) => {
+
+
+
+  try {
+
+    const { today } = req.params;
+    const filePath = `TimeStamp/${today}.txt`;
+
+    const data = await fs.readFileSync(filePath, 'utf8');
+
+    res.status(200).send(data);
+
+  } catch (error) {
+
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+
+  }
+
+
+});
+
+
+
+
+
+// let data = fs.readFileSync(filePath , 'utf8')
+//   res.status(200).send(data)
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+
+  console.log(`App is running in the port  ${PORT}`);
+
+}) // running
+
+
