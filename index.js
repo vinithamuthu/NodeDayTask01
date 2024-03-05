41a2964538c31af67f3203727d6837246f74f7af
vinithamuthu <vinisurya.m@gmail.com>
2024-03-05 20:43:44 +0530

    update

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const PORT = 4000;  // http://localhost:4000/grtProducts
 
 app.get('/', (req, res) => {
 
-  res.status(200).json({ message: 'hai my dear team, we will become a FSD Developer' })
+  // res.status(200).json({ message: 'hai my dear team, we will become a FSD Developer' }) 
 
+  res.status(200).send(`<div style = "background-color:red; color:white " > <h1> Welcome to my team   </h1> </div>`)
+     
 })
 
 app.get('/get-data', (req, res) => {
@@ -34,50 +36,50 @@ app.get('/get-data', (req, res) => {
 
 
 
-app.get('/write', async (req, res) => {
+app.get('/write', async (req,res) => {
 
   try {
 
     let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
-    console.log(('today', today));
-    const filePath = `TimeStamp/${today}.txt`
-    await fs.writeFileSync(filePath, `${today}`, 'utf8')
-    res.status(200).send(`File ${today}.txt created successfully`)
-
+console.log(('today' , today ));
+const filePath = `TimeStamp/${today}.txt`
+await fs.writeFileSync(filePath, `${today}` , 'utf8' )
+res.status(200).send(`File ${today}.txt created successfully`)
+    
   } catch (error) {
 
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
-
+    
   }
 
 })
 
 
-app.get('/read/:today', async (req, res) => {
+app.get('/read', async (req,res) => {
 
 
 
-  try {
+try {
 
-    const { today } = req.params;
-    const filePath = `TimeStamp/${today}.txt`;
+  // const { filename } = req.params;
+ 
 
-    const data = await fs.readFileSync(filePath, 'utf8');
+    const filePath = `TimeStamp/${today}.txt`
+
+    const data = await fs.readFile(filePath, 'utf8');
 
     res.status(200).send(data);
-
-  } catch (error) {
-
-    console.error('Error:', error);
+  
+} catch (error) {
+  console.error('Error:', error);
     res.status(500).send('Internal Server Error');
 
-  }
+
+}
 
 
-});
-
-
+})
 
 
 
