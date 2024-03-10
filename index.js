32069ebc42b0e9e18a66168d048b9a8fe0aab6d3
vinithamuthu <vinisurya.m@gmail.com>
2024-03-10 23:39:00 +0530

    fix: read feature

--- index.js
+++ index.js
@@ -3,99 +3,62 @@ import fs from 'fs';
 import { format } from "date-fns";
 
 const app = express();
-
-// app.use() // middleware
-
 const PORT = 4000;  // http://localhost:4000/grtProducts
 
-
 app.get('/', (req, res) => {
-
   // res.status(200).json({ message: 'hai my dear team, we will become a FSD Developer' }) 
-
-  res.status(200).send(`<div style = "background-color:red; color:white " > <h1> Welcome to my team   </h1> </div>`)
-     
+  res.status(200).send(`<div style = "background-color:red; color:white " > <h1>   </h1> </div>`)
 })
 
 app.get('/get-data', (req, res) => {
-
   res.status(200).json({ message: "data", data: { name: "hello world" } })
-
 })
 
-
-// app.get('/write', (req,res) => {
-// let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
-// console.log(('today' , today ));
-// const filePath = `TimeStamp/${today}.txt`
-// fs.writeFileSync(filePath, `${today}` , 'utf8' )
-// let data = fs.readFileSync(filePath , 'utf8')
+// app.get('/write', (req, res) => {
+//   let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
+//   console.log(('today', today));
+//   const filePath = `TimeStamp/${today}.txt`
+//   fs.writeFileSync(filePath, `${today}`, 'utf8')
+//   let data = fs.readFileSync(filePath, 'utf8')
 //   res.status(200).send(data)
-
 // })
 
-
-
-app.get('/write', async (req,res) => {
+app.get('/write', async (req, res) => {
 
   try {
 
     let today = format(new Date(), 'dd-mm-yyyy-HH-mm-ss')
-console.log(('today' , today ));
-const filePath = `TimeStamp/${today}.txt`
-await fs.writeFileSync(filePath, `${today}` , 'utf8' )
-res.status(200).send(`File ${today}.txt created successfully`)
-    
+    console.log(('today', today));
+    const filePath = `TimeStamp/${today}.txt`
+    await fs.writeFileSync(filePath, `${today}`, 'utf8')
+    res.status(200).send(`File ${today}.txt created successfully`)
+
   } catch (error) {
 
     console.error('Error:', error);
     res.status(500).send('Internal Server Error');
-    
+
   }
 
 })
 
-
-app.get('/read', async (req,res) => {
-
-
-
-try {
-
-  // const { filename } = req.params;
- 
-
-    const filePath = `TimeStamp/${today}.txt`
-
-    const data = await fs.readFile(filePath, 'utf8');
-
+app.get('/read/:today', async (req, res) => {
+  try {
+    const { today } = req.params;
+    const filePath = `./TimeStamp/${today}.txt`
+    const data = fs.readFileSync(filePath, 'utf8');
     res.status(200).send(data);
-  
-} catch (error) {
-  console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-
-
-}
-
-
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send(error);
+  }
 })
 
-
-
 // let data = fs.readFileSync(filePath , 'utf8')
-//   res.status(200).send(data)
-
-
-
-
-
-
+// res.status(200).send(data)
 
 app.listen(PORT, () => {
-
   console.log(`App is running in the port  ${PORT}`);
-
 }) // running
 
 
